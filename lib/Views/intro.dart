3fb328bd1494f212bffeba7887592774978166eb
Macbook de MadeInWeb <mobile@madeinweb.app>
2023-02-16 10:29:57 -0300

    fix create account intro

--- lib/Views/intro.dart
+++ lib/Views/intro.dart
@@ -12,85 +12,78 @@ class WelcomeWidget extends StatefulWidget {
 class _WelcomeWidgetState extends State<WelcomeWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-          backgroundColor: Color.fromARGB(255, 42, 43, 61),
-          body: Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 32, bottom: 16),
-                    child: Text(
-                      "Welcome to",
-                      style: TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.normal),
-                    ),
-                  ),
-                  Text(
-                    "Pokedex",
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 42, 43, 61),
+        body: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 32, bottom: 16),
+                  child: Text(
+                    "Welcome to",
                     style: TextStyle(
-                        fontSize: 32.0,
-                        color: Color.fromARGB(255, 255, 187, 14),
-                        fontWeight: FontWeight.bold),
+                        fontSize: 14.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.normal),
                   ),
-                  Spacer(
-                    flex: 2,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/poke_intro_img.png",
-                        height: 260,
-                      ),
-                    ],
-                  ),
-                  Spacer(
-                    flex: 2,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () => {},
-                        child: Text("Create Account"),
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            primary: Color.fromARGB(255, 255, 187, 14),
-                            onPrimary: Color.fromARGB(255, 42, 43, 61),
-                            textStyle: TextStyle(fontWeight: FontWeight.bold)),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 16, bottom: 32),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Already have an account? ",
-                          style: TextStyle(
-                              fontSize: 12.0,
-                              color: Colors.grey,
-                              fontWeight: FontWeight.normal),
-                        ),
-                        Text(
-                          "Login",
-                          style: TextStyle(
-                              fontSize: 12.0,
-                              color: Color.fromARGB(255, 255, 187, 14),
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                ),
+                Text(
+                  "Pokedex",
+                  style: TextStyle(
+                      fontSize: 32.0,
+                      color: Color.fromARGB(255, 255, 187, 14),
+                      fontWeight: FontWeight.bold),
+                ),
+                Spacer(
+                  flex: 2,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/poke_intro_img.png",
+                      height: 260,
                     ),
+                  ],
+                ),
+                Spacer(
+                  flex: 2,
+                ),
+                ElevatedButton(
+                  onPressed: () => {},
+                  child: Text("Create Account"),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      primary: Color.fromARGB(255, 255, 187, 14),
+                      onPrimary: Color.fromARGB(255, 42, 43, 61),
+                      textStyle: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 32),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account? ",
+                        style: TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.normal),
+                      ),
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                            fontSize: 12.0,
+                            color: Color.fromARGB(255, 255, 187, 14),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                ],
-              ))),
-    );
+                ),
+              ],
+            )));
   }
 }
